904c807bca9bd9cdf3352d4f7bdf5c4c79d22dd9
sushilsah <129074365+sushilsah@users.noreply.github.com>
2023-09-22 10:42:55 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,59 @@
+import telnetlib
+import time
+
+
+IPAdd = input("Type your IP Address to Telnet: ")
+UN = input("Type Username to access the router: ")
+PW =  input("Password for Username: ")
+EPW =  input("Type Password Preivilege: ")
+TA = telnetlib.Telnet(IPAdd)
+    
+#Credentials
+
+TA.read_until("Username: ")
+TA.write(UN+"\n")
+TA.read_until("Password: ")
+TA.write(PW+"\n")
+TA.write("enable\n")
+TA.write(EPW+"\n")
+TA.write("conf t\n")
+time.sleep(3)
+
+#Configuration Commands
+Router_Ip_List = ["192.168.228.101","192.168.228.102","192.168.228.103"]
+print(Router_Ip_List)
+RConfig = input("Do you want to configure Router?")
+
+while RConfig.upper() == "YES":
+    PAns =  input("Do you want to configure Routing Protocol?\nA. Yes\n ")
+    while PAns.upper() == "A":
+        Protocol = input("which protocol you want to configure?\nA. RIP \nB OSPF\n")
+        if Protocol.upper() == "A":
+            TA.write("router rip")
+            RNID = raw_input("PLease Enter the network ID: ")
+            TA.write("network "+RNID+"\n")
+            TA.write("exit")
+        
+        
+        elif Protocol.upper() == "B":
+            TA.write("router ospf 10")
+            RNID = input("PLease Enter the network ID: ")
+            TA.write("network "+RNID+" 0.0.0.255 area 0\n")
+            TA.write("exit")
+            
+        else:
+            
+            print("Please select correct protocol!!!")
+   
+
+print("Configuration Successful!!!")
+            
+           
+
+#Logout and close the session
+    
+TA.write("end\n")
+TA.write("exit\n")
+time.sleep(3)
+TA = TA.read_all()
+time.sleep(3)
